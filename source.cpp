7b7d3e3d8095807b4ed81e8fd9bd6962833590b6
Cornea Cristian <cristi.1990.cristi@gmail.com>
2026-03-22 19:44:27 +0200

    Simplify sample source runner

--- source.cpp
+++ source.cpp
@@ -1,16 +1,6 @@
-#include <print>
-
 #include "utf8_ranges_tests.hpp"
-
-using namespace utf8_ranges;
-using namespace utf8_ranges::literals;
 
 int main()
 {
 	run_utf8_ranges_tests();
-	
-
-	auto sv = "Hello, 世界!"_utf8_sv;
-
-	std::println("{}", sv.reversed_chars() | std::ranges::to<utf8_string>());
 }
