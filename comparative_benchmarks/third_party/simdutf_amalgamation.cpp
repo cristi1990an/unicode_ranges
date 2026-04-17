8b3511bf6e529c376ff783c37fedcae901401e55
Cornea Cristian <cristi.1990.cristi@gmail.com>
2026-04-18 01:53:24 +0300

    Silence simdutf extra-semi warnings

--- comparative_benchmarks/third_party/simdutf_amalgamation.cpp
+++ comparative_benchmarks/third_party/simdutf_amalgamation.cpp
@@ -1,1 +1,15 @@
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wextra-semi"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wextra-semi"
+#endif
+
 #include <simdutf.cpp>
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
